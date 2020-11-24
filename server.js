e5fdf9b07643c5c6100f38f240145e62b3c21a3a
pablo <pglez82@gmail.com>
2020-11-24 18:23:02 +0100

    finishing the server

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require("express")
 const mongoose = require("mongoose")
 const routes = require("./routes") 
 
+function connect(){
+    //We need to change this connection from localhost we put everything in docker-compose
+    mongoose.connect("mongodb://mongoserver:27017/bltappdb", { useNewUrlParser: true }).then(() => {
+        const app = express()
+        app.use(express.json())
+        app.use("/api", routes)
+        app.listen(5000, () => {
+            console.log("Server has started!")
+        })
+    })
+}
 
-// Connect to MongoDB database
-mongoose.connect("mongodb://mongoserver:27017/bltappdb", { useNewUrlParser: true }).then(() => {
-    const app = express()
-    app.use(express.json())
-    app.use("/api", routes)
-	app.listen(5000, () => {
-		console.log("Server has started!")
-	})
-})
+// Connect to MongoDB database, the wait is for giving time to mongodb to finish loading
+setTimeout(connect,5000)
 
